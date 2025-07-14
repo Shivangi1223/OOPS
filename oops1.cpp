e2cbf46bf030b6d343151a8a5af5e21703837b71
Shivangi1223 <singhshivangi21805@gmail.com>
2025-07-14 19:36:25 +0530

    new files

---
+++ oops1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+class First{
+      
+    //Properties -->>
+    int health;
+    char level;
+};
+
+int main(){
+    
+    //Creation of object -->>
+    First f1;
+    cout<<"size is : "<<sizeof(f1)<<endl;
+    return 0;
+
+}
