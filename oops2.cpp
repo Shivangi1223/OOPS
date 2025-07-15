2d9a721059c29703fcb2c9b71d12f08030ad4140
Shivangi1223 <singhshivangi21805@gmail.com>
2025-07-15 22:59:08 +0530

    updated file

---
+++ oops2.cpp
@@ -0,0 +1,15 @@
+// "If the class is empty, then let us see what the output will be."
+
+#include<iostream>
+using namespace std;
+
+class Shiv{
+
+};
+
+
+int main(){
+   Shiv s1;
+   cout<<"size is : "<<sizeof(s1)<<endl;
+   return 0;
+}
