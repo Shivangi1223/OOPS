1d02bb165c7251bae36359722adcc608e28bb87f
Shivangi1223 <singhshivangi21805@gmail.com>
2025-07-15 23:35:35 +0530

    updated

--- oops3.cpp
+++ oops3.cpp
@@ -7,20 +7,77 @@
 */
 
 
+// 1. Public Access Modifier -->>
+
+// #include<iostream>
+// using namespace std;
+
+//     class Shivi{
+//     public:
+//     int health = 55;
+//     char level = 'A';
+//      };
+
+// int main(){
+    
+//     Shivi shivangi;
+//     cout<<"health is: "<<shivangi.health<<endl;
+//     cout<<"level is: "<<shivangi.level<<endl;
+
+//     return 0;
+// }
+
+
+// 2. Private Access Modifier -->>
+//"If a class has some private members, we use getter and setter methods to access them from outside the class."
+
 #include<iostream>
 using namespace std;
 
-    class Shivi{
+  class First {
+
+    private:
+    int health;
+
     public:
-    int health = 55;
-    char level = 'A';
-     };
+    char level;
+
+    void print(){
+        cout<<level<<endl;
+    }
+
+    int getHealth(){    
+        return health;
+    }
+
+    char getLevel(){
+        return level;
+    }
+
+    void setHealth (int h){
+        health = h;
+    }
+
+    void setLevel (char ch){
+        level = ch;
+    }
+
+
+  };
 
 int main(){
+
+    First Shivi;
     
-    Shivi shivangi;
-    cout<<"size is: "<<shivangi.health<<endl;
-    cout<<"size is: "<<shivangi.level<<endl;
+    //use get -->>
+    cout<<"Shivi Health is: "<<Shivi.getHealth()<<endl;
+
+    // use set -->>
+    Shivi.setHealth(70);
+    Shivi.level = 'A';
+    cout<<"My health is: "<<Shivi.getHealth()<<endl;
+    cout<<"My level is: "<<Shivi.level<<endl;
+
 
     return 0;
 }
