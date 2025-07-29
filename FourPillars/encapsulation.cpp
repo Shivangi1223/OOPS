96498edb51381deab698cdd3885448f67ff715e2
Shivangi1223 <singhshivangi21805@gmail.com>
2025-07-29 07:55:30 +0530

    new files

---
+++ FourPillars/encapsulation.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+class student{
+    public:
+    int age;
+    int height;
+
+    public:
+    int getAge(){
+        return this->age;
+    }
+};
+ 
+int main(){
+
+    student first;
+
+    cout<<"All okk"<<endl;
+
+    
+    return 0;
+}
---
+++ FourPillars/inheritance.cpp
@@ -0,0 +1,222 @@
+// #include<iostream> 
+// using namespace std;
+
+// // FIRST WE WILL CREATE A HUMAN CLASS -->> PARENT CLASS 
+// class Human{
+//       public:
+//       int height;
+//       int weight;
+//       private:
+//       int age;
+
+//       public:
+//       int getAge(){
+//         return this->age;
+//       }
+//       void setWeight(int w){
+//         this->weight = w;
+//       }
+// };
+
+
+// // Public mode -->
+
+// class Male: public Human{
+//       public:
+//       string color;
+
+//       void say(){
+//         cout<< "Male Say's : "<<endl;
+//       }
+// };
+
+
+
+
+// int main(){
+
+
+
+//      Male m1;
+//      cout<<m1.height<<endl;
+
+  /*
+    Male object1;
+    // cout<< object1.age <<endl;    // Human age is inaccessible bcz age is a private member of human.... >> If anything is marked private in base class and u are inheriting it in a public way, Then it will be not accessible :)
+    cout<< object1.weight<<endl;
+    cout<<object1.height<<endl;
+    cout<<object1.color<<endl;
+
+
+    object1.setWeight(54);
+    cout<<object1.weight<<endl;
+
+    object1.say();    
+    return 0;
+}
+
+*/
+
+
+
+// Protected modifier -->>
+
+// #include<iostream> 
+// using namespace std;
+
+// // FIRST WE WILL CREATE A HUMAN CLASS -->> PARENT CLASS 
+// class Human{
+//       public:
+//       int height;
+//       int weight;
+//       private:
+//       int age;
+
+//       public:
+//       int getAge(){
+//         return this->age;
+//       }
+//       void setWeight(int w){
+//         this->weight = w;
+//       }
+// };
+
+
+// // Public mode -->
+
+// class Male: protected Human{
+//       public:
+//       string color;
+
+//       void say(){
+//         cout<< "Male Say's : "<<endl;
+//       }
+
+//       int getHeight(){
+//         return this->height;
+//       }
+// };
+
+
+
+
+// int main(){
+
+
+
+//      Male m1;
+//      cout<<m1.getHeight()<<endl;
+
+
+//      return 0;
+// }
+
+
+
+// Private modifier -->>
+
+
+// #include<iostream> 
+// using namespace std;
+
+// // FIRST WE WILL CREATE A HUMAN CLASS -->> PARENT CLASS 
+// class Human{
+//       public:
+//       int height;
+//       int weight;
+//       private:
+//       int age;
+
+//       public:
+//       int getAge(){
+//         return this->age;
+//       }
+//       void setWeight(int w){
+//         this->weight = w;
+//       }
+// };
+
+
+// // Public mode -->
+
+// class Male: private Human{    // Height will become private bcz when supaer class is public, and the mode of inheritance is private, then answer will be private :)
+//       public:
+//       string color;
+
+//       void say(){
+//         cout<< "Male Say's : "<<endl;
+//       }
+
+//       int getHeight(){
+//         return this->height;
+//       }
+// };
+
+
+
+
+// int main(){
+
+
+
+//      Male m1;
+//      cout<<m1.getHeight()<<endl;   // It is not accessible bcz height is a private member -->>   but if i try to aceess it through get/set, then it will run -->>
+
+
+//      return 0;
+// }
+
+
+// 2.) Combination -->>
+#include<iostream> 
+using namespace std;
+
+// FIRST WE WILL CREATE A HUMAN CLASS -->> PARENT CLASS 
+class Human{
+      protected:
+      int height;
+
+      public:
+      int weight;
+      private:
+      int age;
+
+      public:
+      int getAge(){
+        return this->age;
+      }
+      void setWeight(int w){
+        this->weight = w;
+      }
+};
+
+
+// Public mode -->
+
+class Male: public Human{    
+      public:
+      string color;
+
+      void say(){
+        cout<< "Male Say's : "<<endl;
+      }
+
+      int getHeight(){
+        return this->height;
+      }
+};
+
+
+
+
+int main(){
+
+
+
+     Male m1;
+    //  cout<<m1.height<<endl;
+     cout<<m1.getHeight()<<endl;   // It is not accessible bcz height is a protected member -->>   but if i try to aceess it through get/set, then it will run -->>
+
+
+     return 0;
+} 
