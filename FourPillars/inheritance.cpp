915dd084408393c1f3cb007e2c4007b21127f324
Shivangi1223 <singhshivangi21805@gmail.com>
2025-07-30 00:11:00 +0530

    updated file

--- FourPillars/inheritance.cpp
+++ FourPillars/inheritance.cpp
@@ -168,55 +168,217 @@
 
 
 // 2.) Combination -->>
-#include<iostream> 
-using namespace std;
+// #include<iostream> 
+// using namespace std;
 
-// FIRST WE WILL CREATE A HUMAN CLASS -->> PARENT CLASS 
-class Human{
-      protected:
-      int height;
+// // FIRST WE WILL CREATE A HUMAN CLASS -->> PARENT CLASS 
+// class Human{
+//       protected:
+//       int height;
 
-      public:
-      int weight;
-      private:
-      int age;
+//       public:
+//       int weight;
+//       private:
+//       int age;
 
-      public:
-      int getAge(){
-        return this->age;
-      }
-      void setWeight(int w){
-        this->weight = w;
-      }
-};
-
-
-// Public mode -->
-
-class Male: public Human{    
-      public:
-      string color;
-
-      void say(){
-        cout<< "Male Say's : "<<endl;
-      }
-
-      int getHeight(){
-        return this->height;
-      }
-};
+//       public:
+//       int getAge(){
+//         return this->age;
+//       }
+//       void setWeight(int w){
+//         this->weight = w;
+//       }
+// };
 
 
+// // Public mode -->
 
+// class Male: public Human{    
+//       public:
+//       string color;
 
-int main(){
+//       void say(){
+//         cout<< "Male Say's : "<<endl;
+//       }
+
+//       int getHeight(){
+//         return this->height;
+//       }
+// };
 
 
 
-     Male m1;
-    //  cout<<m1.height<<endl;
-     cout<<m1.getHeight()<<endl;   // It is not accessible bcz height is a protected member -->>   but if i try to aceess it through get/set, then it will run -->>
+
+// int main(){
 
 
-     return 0;
-} 
+
+//      Male m1;
+//     //  cout<<m1.height<<endl;
+//      cout<<m1.getHeight()<<endl;   // It is not accessible bcz height is a protected member -->>   but if i try to aceess it through get/set, then it will run -->>
+
+
+//      return 0;
+// } 
+
+
+// When both are Protected -->>
+// #include<iostream> 
+// using namespace std;
+
+// // FIRST WE WILL CREATE A HUMAN CLASS -->> PARENT CLASS 
+// class Human{
+//       protected:
+//       int height;
+
+//       public:
+//       int weight;
+//       private:
+//       int age;
+
+//       public:
+//       int getAge(){
+//         return this->age;
+//       }
+//       void setWeight(int w){
+//         this->weight = w;
+//       }
+// };
+
+
+// // Public mode -->
+
+// class Male: protected Human{    
+//       public:
+//       string color;
+
+//       void say(){
+//         cout<< "Male Say's : "<<endl;
+//       }
+
+//       int getHeight(){
+//         return this->height;
+//       }
+// };
+
+
+
+
+// int main(){
+
+
+
+//      Male m1;
+    //  cout<<m1.height<<endl; // height is protected member u can not access it -->  we can access it "inside class and child class"
+    
+//      return 0;
+// } 
+
+
+
+// #include<iostream> 
+// using namespace std;
+
+// // FIRST WE WILL CREATE A HUMAN CLASS -->> PARENT CLASS 
+// class Human{
+//       protected:
+//       int height;
+
+//       public:
+//       int weight;
+//       private:
+//       int age;
+
+//       public:
+//       int getAge(){
+//         return this->age;
+//       }
+//       void setWeight(int w){
+//         this->weight = w;
+//       }
+// };
+
+
+// // Public mode -->
+
+// class Male: private Human{    
+//       public:
+//       string color;
+
+//       void say(){
+//         cout<< "Male Say's : "<<endl;
+//       }
+
+//       int getHeight(){
+//         return this->height;
+//       }
+// };
+
+
+
+
+// int main(){
+
+
+
+//      Male m1;
+//     //  cout<<m1.height<<endl; // height is protected member u can not access it directly :)
+    
+//      return 0;
+// } 
+
+
+// 3.) Private access modifier ->
+
+// In private mode you can inherit it in any mode be in public, private and protected, in all cases it is not accessible it will give error ..... Not inside the class and lso not outside the class :)
+// #include<iostream> 
+// using namespace std;
+
+// // FIRST WE WILL CREATE A HUMAN CLASS -->> PARENT CLASS 
+// class Human{
+//       private:
+//       int height;
+
+//       public:
+//       int weight;
+//       private:
+//       int age;
+
+//       public:
+//       int getAge(){
+//         return this->age;
+//       }
+//       void setWeight(int w){
+//         this->weight = w;
+//       }
+// };
+
+
+// // Public mode -->
+
+// class Male: public Human{    
+//       public:
+//       string color;
+
+//       void say(){
+//         cout<< "Male Say's : "<<endl;
+//       }
+
+//       int getHeight(){
+//         return this->height;
+//       }
+// };
+
+
+
+
+// int main(){
+
+
+
+//      Male m1;
+//      cout<<m1.height<<endl;
+
+    
+//      return 0;
+// } 
